42bd7c1acfdfa05182921b37c11ed7fce54380c9
Josue Cruz Barragan <ing.cruzbarragan@hotmail.com>
2024-02-28 17:09:01 -0600

    Se crea el GET a la funcion "home" para usar la API

--- quotes/views.py
+++ quotes/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    # pk_56410da2968a4043bd55c65f7465ee84
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_56410da2968a4043bd55c65f7465ee84")
+    try:
+        api = json.loads(api_request.content)
+    except:
+        api = "Error... "
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
