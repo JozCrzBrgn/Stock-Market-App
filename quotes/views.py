8a4e7de5f4678e63c96e1d7d76067990a8d922f4
Josue Cruz Barragan <ing.cruzbarragan@hotmail.com>
2024-02-28 21:28:24 -0600

    Se agrega metodo POST al home para los datos del formulario

--- quotes/views.py
+++ quotes/views.py
@@ -4,15 +4,17 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-
-    # pk_56410da2968a4043bd55c65f7465ee84
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_56410da2968a4043bd55c65f7465ee84")
-    try:
-        api = json.loads(api_request.content)
-    except:
-        api = "Error... "
-
-    return render(request, 'home.html', {'api':api})
+    if request.method == "POST":
+        ticker = request.POST['ticker']
+        api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token=pk_56410da2968a4043bd55c65f7465ee84")
+        try:
+            api = json.loads(api_request.content)
+        except:
+            api = "Error... "
+        return render(request, 'home.html', {'api':api})
+    else:
+        return render(request, 'home.html', {'ticker':"Enter a Ticker Symbol Above..."})
+    
 
 def about(request):
     return render(request, 'about.html', {})
